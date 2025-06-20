f0ac71fea1c04247edd4085c9935ac7c242326a2
Diego Bravo <brzoale2510@gmail.com>
2025-06-19 19:41:13 -0700

    Atributos de Arboles

--- src/App.java
+++ src/App.java
@@ -11,8 +11,25 @@ public class App {
         ab.insert(54);
         ab.insert(14);
         ab.insert(19);
-        ab.printPreOrder();
-        ab.printPostOrder();
+        System.out.println("Peso del Arbol: "+ ab.getSize());
+        System.out.println("Altura del Arbol: " + ab.getHeight());
+        System.out.println("");
+        System.out.println("Arbol InOrder:");
         ab.printInOrder();
+        System.out.println("Arbol InOrder con alturas:");
+        ab.printInOrderWithHeigth();
+        System.out.println("Arbol InOrder con factor de equilibrio:");
+        ab.printInOrderWithBalanceFactor();
+        System.out.println("");
+        System.out.println("Arbol está equilibrado: " + ab.isBalanced());
+        System.out.println("Existe el valor 15: " + ab.contains(15));
+        System.out.println("Agregando el valor: "+ 15);
+        ab.insert(15);
+        System.out.println("Arbol está equilibrado: " + ab.isBalanced());
+        System.out.println("");
+        System.out.println("Arbol InOrder con factor de equilibrio:");
+        ab.printInOrderWithBalanceFactor();
+        System.out.println("Nodos desequilibrados: ");
+        ab.getUnbalancedNodesCount();
     }
 }
--- src/materia/controllers/BinaryTree.java
+++ src/materia/controllers/BinaryTree.java
@@ -5,12 +5,15 @@ import materia.models.Node;
 public class BinaryTree {
 
     private Node root;
+    private int size;
 
     public BinaryTree() {
         this.root = null;
+        this.size = 0;
     }
 
     public void insert(int value) {
+        size++;
         root = insertRec(root, value);
     }
 
@@ -78,4 +81,112 @@ public class BinaryTree {
             printInOrderRec(node.getRight());
         }
     }
+
+    public void printInOrderWithHeigth(){
+        printInOrderWithHeightRec(root);
+        System.out.println("");
+    }
+
+    public void printInOrderWithHeightRec(Node node){
+        if (node != null){
+            printInOrderWithHeightRec(node.getLeft());
+            int height = getHeightRec(node);
+            System.out.print(node.getValue() + "(h="+height+") ");
+            printInOrderWithHeightRec(node.getRight());
+        }
+    }
+
+    public int getBalanceFactor(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        return getHeightRec(node.getLeft()) - getHeightRec(node.getRight());
+    }
+
+    public void printInOrderWithBalanceFactor() {
+        printInOrderWithBalanceFactorRec(root);
+        System.out.println("");
+    }
+
+    public void printInOrderWithBalanceFactorRec(Node node) {
+        if (node != null) {
+            printInOrderWithBalanceFactorRec(node.getLeft());
+            int balanceFactor = getBalanceFactor(node);
+            System.out.print(node.getValue() + "(bf=" + balanceFactor + ") ");
+            printInOrderWithBalanceFactorRec(node.getRight());
+        }
+    }
+
+    public boolean search(int value) {
+        return searchRec(root, value);
+    }
+
+    public boolean searchRec(Node node, int value) {
+        if (node == null) {
+            return false;
+        }
+        if (node.getValue() == value) {
+            return true;
+        }
+        if (value < node.getValue()) {
+            return searchRec(node.getLeft(), value);
+        } else {
+            return searchRec(node.getRight(), value);
+        }
+    }
+
+    public int getHeight() {
+        return getHeightRec(root);
+    }
+
+    public int getHeightRec(Node node) {
+        if (node == null) {
+            return 0; 
+        }
+        int leftHeight = getHeightRec(node.getLeft());
+        int rightHeight = getHeightRec(node.getRight());
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    public boolean isBalanced() {
+        return isBalancedRec(root);
+    }
+
+    private boolean isBalancedRec(Node node) {
+        if (node == null) {
+            return true;
+        }
+        int balanceFactor = getBalanceFactor(node);
+        if (Math.abs(balanceFactor) > 1) {
+            return false;
+        }
+        return isBalancedRec(node.getLeft()) && isBalancedRec(node.getRight());
+    }
+
+    public void getUnbalancedNodesCount() {
+        getUnbalancedNodesCountRec(root);
+        System.out.println("");
+    }
+
+    private void getUnbalancedNodesCountRec(Node node) {
+        if (node != null) {
+            int balanceFactor = getBalanceFactor(node);
+            if (Math.abs(balanceFactor) > 1 || Math.abs(balanceFactor) <-1) {
+                System.out.print(node.getValue() + "(bf=" + balanceFactor + ") ");
+            }
+            getUnbalancedNodesCountRec(node.getLeft());
+            getUnbalancedNodesCountRec(node.getRight());
+        }
+    }
+
+    public boolean contains(int value) {
+        return searchRec(root, value);
+    }
+    
+
+    public int getSize() {
+        return size;
+    }
+
+
 }
